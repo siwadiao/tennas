5fa9b248257f55e0762182186b2d86736bd8809a
siwadiao <buoihandoan64147@gmail.com>
2025-08-03 11:14:03 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://helloworld-org.jimdosite.com/?gad_source=2&gad_campaignid=22805842237&gclid=CjwKCAjw7rbEBhB5EiwA1V49nX5-WNDnq6xjkhfbmee4F1iapTRCEupjHvigXaenj6XyaOxO3hRw1xoCOpoQAvD_BwE";
+		var url="";
 		window.location=url;
 	}
 }
@@ -11,4 +11,5 @@ var num=0;
 function getLoad()
 {
 	num++;
+
 }
